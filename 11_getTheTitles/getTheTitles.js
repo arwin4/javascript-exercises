d2beccc8b95a694b93b9b20d0aa372a12438b1df
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-02-20 14:07:55 +0100

    Make exercises 8 through 12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,30 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
-};
+// const getTheTitles = function(booklist) {
+//     let titles = []
+//     for (let key in booklist) {
+//         titles.push(booklist[key].title)
+//     }
+//     return titles;
+// };
+
+
+// Solution with .map
+
+const getTheTitles = function(booklist) {
+  return booklist.map(book => book.title);
+}
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,42 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
 
+const findTheOldest = function (people) {
+  let oldestAge = 0;
+  let oldestPerson;
+  let currentAge;
+  people.reduce((sum, person, index) => {
+    // property name must be a string, can't use ! operator
+    if ("yearOfDeath" in person === false) {
+      let currentYear = new Date().getFullYear();
+      currentAge = (currentYear - person.yearOfBirth);
+    } else {
+      currentAge = (person.yearOfDeath - person.yearOfBirth);
+    }
+    if (currentAge > oldestAge) {
+      oldestAge = currentAge;
+      oldestPerson = people[index];
+    }
+  }, 0)
+  return oldestPerson;
 };
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
